6318b6595e6b6192c076f27527538920561bb374
Maximilian Karacsony <maximilian.karacsony@mail.de>
2017-08-05 18:13:51 +0200

    Add basic logging

--- app/index.js
+++ app/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 
+const { logger } = require('../tools/logger');
 const { authRouter } = require('../routes/auth');
 
 const app = express();
 
+app.use(morgan('dev', { stream: logger.stream }));
 app.use('/auth', authRouter);
 app.get('/', (req, res) => {
   res.send('Hello, World!');
---
+++ tools/logger.js
@@ -0,0 +1,23 @@
+const winston = require('winston');
+
+const logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+      json: false,
+      colorize: true,
+    }),
+  ],
+  exitOnError: false,
+});
+
+logger.stream = {
+  write(message) {
+    logger.info(message.slice(0, -1));
+  },
+};
+
+module.exports = {
+  logger,
+};
