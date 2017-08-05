1241fdd4a03b0bc96446f4a658a8a73ca4a31183
Maximilian Karacsony <maximilian.karacsony@mail.de>
2017-08-05 20:15:22 +0200

    Add compression for incoming requests

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const compression = require('compression');
 
 const { logger } = require('../tools/logger');
 const { authRouter } = require('../routes/auth');
 
 const app = express();
 
+app.use(compression());
 app.use(morgan('dev', { stream: logger.stream }));
 app.use('/auth', authRouter);
 app.get('/', (req, res) => {
