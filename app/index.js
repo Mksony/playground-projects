170c86c222bd9befa96235cd212909f32c390a6f
Maximilian Karacsony <maximilian.karacsony@mail.de>
2017-08-05 21:11:18 +0200

    Add security http headers

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const compression = require('compression');
+const helmet = require('helmet');
 
 const { logger } = require('../tools/logger');
 const { authRouter } = require('../routes/auth');
 
 const app = express();
 
+app.use(helmet());
 app.use(compression());
 app.use(morgan('dev', { stream: logger.stream }));
 app.use('/auth', authRouter);
